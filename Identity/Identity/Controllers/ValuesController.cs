5afae6afcfeda07e0a09c9cb2a0091946c43553d
altunbas-huseyin <altunbas.huseyin@gmail.com>
2017-05-07 20:57:10 +0300

    cln

--- Identity/Identity/Controllers/ValuesController.cs
+++ Identity/Identity/Controllers/ValuesController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.OData;
 
 namespace Identity.Controllers
 {
+
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
