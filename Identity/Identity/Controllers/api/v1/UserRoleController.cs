a5e3afccf8df3f55809a37654e3e9da02d7ee9b2
altunbas-huseyin <altunbas.huseyin@gmail.com>
2017-05-25 18:06:24 +0300

    UserRoleRepo oluşturuldu.

---
+++ Identity/Identity/Controllers/api/v1/UserRoleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Identity.Controllers.api.v1
+{
+    [Route("api/[controller]")]
+    public class UserRoleController : Controller
+    {
+     
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string UserId, String RoleId)
+        {
+        }
+
+        
+        // DELETE api/values/5
+        [HttpDelete]
+        public void Delete(string RoleId)
+        {
+        }
+    }
+}
---
+++ Identity/IdentityRepository/UserRoleRepo.cs
@@ -0,0 +1,47 @@
+﻿using IdentityModels.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using IdentityModels.Roles;
+
+namespace IdentityRepository
+{
+    public class UserRoleRepo
+    {
+        private MongoDbRepository<User> userRepository = new MongoDbRepository<User>();
+        private RoleRepo roleRepo = new RoleRepo();
+        private UserRepo userRepo = new UserRepo();
+
+        public bool UserAddRole(string ParentId, string UserId, string RoleId)
+        {
+            User user = userRepo.GetById(ParentId, UserId);
+            if (user == null)
+            { return false; }
+
+            //Rol kullanıcıya daha önce atanmış ise bir işlem yapılmıyor
+            bool result = IsAddedRole(user, RoleId);
+            if (result)
+            { return true; }
+
+            Role role = roleRepo.GetById(ParentId, RoleId);
+            if (user.Role.Count < 1)
+            { user.Role = new List<Role>(); }
+            user.Role.Add(role);
+
+            bool userUpdateResult = userRepo.Update(user);
+            return userUpdateResult;
+        }
+
+        public bool IsAddedRole(User user, string RoleId)
+        {
+            Role role = user.Role.Where(p => p._id == RoleId).First();
+            if (role != null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
